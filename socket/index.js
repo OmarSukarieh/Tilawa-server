285b65b46aadfb537b6a03268863f3c36f26e6b8
Omars.DEV <omarsukareih99@gmail.com>
2022-05-12 16:50:30 +0300

    changes

--- socket/index.js
+++ socket/index.js
@@ -24,7 +24,7 @@ const SocketServer = (server) => {
   });
 
   io.use(async (socket, next) => {
-    const token = socket.handshake.headers.auth;
+    const token = socket.handshake.headers.auth || socket.handshake.auth.token;
     try {
       const decoded = jwt.verify(token, configApp.jwtSecret);
       // ---------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -77,176 +77,98 @@ const SocketServer = (server) => {
         }
         next();
       } else {
-        socket.conn.close();
+        next(new Error("not authorized"));
       }
     } catch (error) {
-      socket.conn.close();
+      next(new Error("not authorized"));
     }
   });
 
-  io.on("connection", (socket) => {
-    // socket.on("join", async () => {
-    //   const token = socket.handshake.headers.auth;
-    //   try {
-    //     const decoded = jwt.verify(token, configApp.jwtSecret);
-    //     if (decoded.type === "user") {
-    //       if (users.has(decoded.id) && users.get(decoded.id).sockets.indexOf(socket.id) === -1) {
-    //         const existingUser = users.get(decoded.id);
-    //         existingUser.sockets = [...existingUser.sockets, ...[socket.id]];
-    //         users.set(decoded.id, existingUser);
-    //         userSockets.set(socket.id, decoded.id);
-    //       } else if (!users.has(decoded.id)) {
-    //         users.set(decoded.id, {
-    //           id: decoded.id,
-    //           sockets: [socket.id],
-    //         });
-    //         userSockets.set(socket.id, decoded.id);
-    //       }
-    //     } else if (decoded.type === "teacher") {
-    //       if (teachers.has(decoded.id) && teachers.get(decoded.id).sockets.indexOf(socket.id) === -1) {
-    //         const existingTeacher = teachers.get(decoded.id);
-    //         existingTeacher.sockets = [...existingTeacher.sockets, ...[socket.id]];
-    //         teachers.set(decoded.id, existingTeacher);
-    //         teacherSockets.set(socket.id, decoded.id);
-    //       } else if (!teachers.has(decoded.id)) {
-    //         teachers.set(decoded.id, {
-    //           id: decoded.id,
-    //           sockets: [socket.id],
-    //         });
-    //         teacherSockets.set(socket.id, decoded.id);
-    //       }
-    //     }
-    //   } catch (err) {
-    //     socket.conn.close();
-    //   }
-    // });
-
+  io.on("connection", (socket, next) => {
     socket.on("disconnect", async () => {
-      const token = socket.handshake.headers.auth;
-      try {
-        const decoded = jwt.verify(token, configApp.jwtSecret);
-        if (decoded.type === "user") {
-          if (userSockets.has(socket.id)) {
-            const user = users.get(userSockets.get(socket.id));
-            user.sockets = user.sockets.filter((sock) => {
-              if (sock !== socket.id) return true;
-              userSockets.delete(sock);
-              return false;
-            });
-            users.set(user.id, user);
-          }
-        } else if (decoded.type === "teacher") {
-          if (teacherSockets.has(socket.id)) {
-            const teacher = teachers.get(teacherSockets.get(socket.id));
-            teacher.sockets = teacher.sockets.filter((sock) => {
-              if (sock !== socket.id) return true;
-              teacherSockets.delete(sock);
-              return false;
-            });
-            teachers.set(teacher.id, teacher);
-          }
-        } else {
-          socket.conn.close();
+      console.log("Disconnect")
+      const typeUser = userSockets.get(socket.id) ? "user" : teacherSockets.get(socket.id) ? "teacher" : null
+
+      if (typeUser === "user") {
+        if (userSockets.has(socket.id)) {
+          const user = users.get(userSockets.get(socket.id));
+          user.sockets = user.sockets.filter((sock) => {
+            if (sock !== socket.id) return true;
+            userSockets.delete(sock);
+            return false;
+          });
+          users.set(user.id, user);
         }
-      } catch (error) {
-        socket.conn.close();
+      } else if (typeUser === "teacher") {
+        if (teacherSockets.has(socket.id)) {
+          const teacher = teachers.get(teacherSockets.get(socket.id));
+          teacher.sockets = teacher.sockets.filter((sock) => {
+            if (sock !== socket.id) return true;
+            teacherSockets.delete(sock);
+            return false;
+          });
+          teachers.set(teacher.id, teacher);
+        }
       }
     });
 
     socket.on("message", async (msg) => {
-      const token = socket.handshake.headers.auth;
-      try {
-        const decoded = jwt.verify(token, configApp.jwtSecret);
-        let teacherId;
-        let userId;
+      const typeUserTeacher = userSockets.get(socket.id) ? { type: "user", id: userSockets.get(socket.id) } : teacherSockets.get(socket.id) ? { type: "teacher", id: teacherSockets.get(socket.id) } : null
 
-        if (decoded.type === "user") {
-          teacherId = msg.teacherId;
-          userId = decoded.id;
-        } else if (decoded.type === "teacher") {
-          teacherId = decoded.id;
-          userId = msg.userId;
-        } else {
-          socket.conn.close();
-        }
+      if (!typeUserTeacher) next(new Error("not authorized"));
 
-        const findChat = await Chat.findOne({
-          where: { teacherId, userId },
-        });
+      let teacherId;
+      let userId;
 
-        let chatId;
-        if (!findChat) {
-          const createChat = await Chat.create({ teacherId, userId });
-          chatId = createChat.id;
-        } else {
-          chatId = findChat.id;
-        }
-
-        let createMessage;
-        if (decoded.type === "user") {
-          createMessage = await Message.create({
-            chatId,
-            message: msg.message,
-            messageType: msg.type,
-            fromUserId: userId,
-          });
-          teachers?.get(teacherId)?.sockets?.map((e) => {
-            if (teacherSockets.has(e)) {
-              io.to(e).emit("receive", createMessage);
-            }
-          });
-        } else if (decoded.type === "teacher") {
-          createMessage = await Message.create({
-            chatId,
-            message: msg.message,
-            messageType: msg.type,
-            fromTeacherId: teacherId,
-          });
-          users?.get(userId)?.sockets?.map((e) => {
-            if (userSockets.has(e)) {
-              io.to(e).emit("receive", createMessage);
-            }
-          });
-        }
-
-      } catch (error) {
-        socket.conn.close();
+      if (typeUserTeacher.type === "user") {
+        teacherId = msg.teacherId;
+        userId = typeUserTeacher.id;
+      } else if (typeUserTeacher.type === "teacher") {
+        teacherId = typeUserTeacher.id;
+        userId = msg.userId;
+      } else {
+        next(new Error("not authorized"));
       }
 
-      // const socketData = userTeacherSocket.get(socket.id);
+      const findChat = await Chat.findOne({
+        where: { teacherId, userId },
+      });
 
-      // const findChat = await Chat.findOne({
-      //   where: { teacherId, userId },
-      // });
+      let chatId;
+      if (!findChat) {
+        const createChat = await Chat.create({ teacherId, userId });
+        chatId = createChat.id;
+      } else {
+        chatId = findChat.id;
+      }
 
-      // let chatId;
-      // if (!findChat) {
-      //   const createChat = await Chat.create({ teacherId, userId });
-      //   chatId = createChat.id;
-      // } else {
-      //   chatId = findChat.id;
-      // }
-
-      // let createMessage;
-      // if (socketData.type === "user") {
-      //   createMessage = await Message.create({
-      //     chatId,
-      //     message: msg.message,
-      //     messageType: msg.type,
-      //     fromUserId: userId,
-      //   });
-      //   if (teachers.has(teacherId)) io.to(teachers.get(teacherId)).emit("receive", createMessage)
-      //   console.log(teachers.has(teacherId), teachers.get(teacherId))
-      // } else if (socketData.type === "teacher") {
-      //   createMessage = await Message.create({
-      //     chatId,
-      //     message: msg.message,
-      //     messageType: msg.type,
-      //     fromTeacherId: teacherId,
-      //   });
-      //   console.log(users.has(userId), users.get(userId))
-      //   if (users.has(userId)) io.to(users.get(userId)).emit("receive", createMessage)
-      // }
+      let createMessage;
+      if (typeUserTeacher.type === "user") {
+        createMessage = await Message.create({
+          chatId,
+          message: msg.message,
+          messageType: msg.type,
+          fromUserId: userId,
+        });
+        teachers?.get(teacherId)?.sockets?.map((e) => {
+          if (teacherSockets.has(e)) {
+            io.to(e).emit("receive", createMessage);
+          }
+        });
+      } else if (typeUserTeacher.type === "teacher") {
+        createMessage = await Message.create({
+          chatId,
+          message: msg.message,
+          messageType: msg.type,
+          fromTeacherId: teacherId,
+        });
+        console.log(users?.get(userId)?.sockets[0], userSockets.has(users?.get(userId)?.sockets[0]))
+        users?.get(userId)?.sockets?.map((e) => {
+          if (userSockets.has(e)) {
+            io.to(e).emit("receive", createMessage);
+          }
+        });
+      }
     });
   });
 };
